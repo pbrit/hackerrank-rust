dd8a300c9986501bab9015fa274f7e011a465302
Pavel Chechetin <paulche@yandex.ru>
2017-07-29 23:54:35 -0700

    grading is ready

--- grading/src/main.rs
+++ grading/src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut number_of_grades = String::new();
+    io::stdin().read_line(&mut number_of_grades).unwrap();
+    let number_of_grades: usize = number_of_grades.trim().parse().unwrap();
+
+    for _ in 0..number_of_grades {
+        let mut grade = String::new();
+
+        io::stdin().read_line(&mut grade).unwrap();
+
+        let grade: u8 = grade.trim().parse().unwrap();
+
+        if grade < 38 {
+            println!("{}", grade);
+        } else {
+            let reminder = grade % 5;
+            let normal_grade = if reminder >= 3 {
+                5 * (grade / 5) + 5
+            } else {
+                grade
+            };
+
+            println!("{}", normal_grade);
+        }
+    }
 }
